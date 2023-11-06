0fc363596cfcf7605e1b62f8b51e577bd93203bc
gjm@pc <tdouguo@163.com>
2023-11-06 11:55:28 +0800

    css 和 js 从html 剥离

--- README.md
+++ README.md
@@ -22,3 +22,11 @@
 | GitHub Release | https://github.com/Molunerfinn/PicGo/releases | All | 国内下载速度可能会慢 |
 | https://cloud.tencent.com/product/cos | https://github.com/Molunerfinn/PicGo/releases 附在更新日志结尾 | All | 感谢 https://cloud.tencent.com/product/cos 提供的赞助支持 |
 | https://mirrors.sdu.edu.cn/ | https://mirrors.sdu.edu.cn/github-release/Molunerfinn_PicGo | All | 感谢 https://mirrors.sdu.edu.cn/ 提供的镜像支持 |
+
+
+# 未完成列表
+
+-[ ] 页面在进行加载是不要一下全加载，而是启动只预加载当前页面+下一个页面（1个页面=当前高度），下滑是预加载下下个页面图片
+-[ ] 在右上角这里显示存储，选择后重新加载，类似当前 public-cdn/f  和 public-cdn/mhimg, 
+-[ ] 界面的图片要兼容图片的 宽度，能自适应移动端和PC端
+-[ ] 可以点击界面 有个放大图片弹出，弹窗右上角关闭，下面有下载按钮 点击即可下载。
---
+++ js/main.js
@@ -0,0 +1,66 @@
+const repoOwner = 'public-cdn';
+const repoName = 'f';
+const supportedImageFormats = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
+const ignoreNames = ["blank_popup_bg.png", "blank_popup_ico.png"];
+
+function isInViewport(element) {
+	const rect = element.getBoundingClientRect();
+	return (
+		rect.bottom >= 0 &&
+		rect.right >= 0 &&
+		rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
+function loadImagesInView() {
+
+	const images = document.querySelectorAll('.images img[lazy="true"]');
+	images.forEach(image => {
+		if (isInViewport(image)) {
+			image.src = image.dataset.src;
+			image.removeAttribute('lazy');
+		}
+	});
+}
+// Load images when scrolling
+document.addEventListener('scroll', loadImagesInView);
+// Also check when the page is resized
+window.addEventListener('resize', loadImagesInView);
+
+// 启动加载图片
+document.addEventListener('DOMContentLoaded', function () {
+	
+	const imagesContainer = document.querySelector('.images');
+
+	function fetchFiles(path = '') {
+		fetch(`https://github-proxy-api.pages.dev/repos/${repoOwner}/${repoName}/contents/${path}`, {
+			headers: {
+				'Content-Type': 'application/json',
+				'x-token': 'public-cdn',
+			}
+		})
+			.then(response => response.json())
+			.then(files => {
+				files.forEach(file => {
+					if (file.type === 'file') {
+
+						if (!ignoreNames.includes(file.name.toLowerCase()) && supportedImageFormats.some(format => file.name.toLowerCase().endsWith(format))) {
+							const img = document.createElement('img');
+							img.src = "https://f.laosepi.cc/" + file.path;
+							img.alt = file.name;
+							img.loading = 'lazy';
+							imagesContainer.appendChild(img);
+						}
+					} else if (file.type === 'dir') {
+						fetchFiles(file.path);
+					}
+				});
+			})
+			.catch(error => console.error('Error fetching files from GitHub:', error));
+	}
+
+	fetchFiles();
+	
+	loadImagesInView();
+});
